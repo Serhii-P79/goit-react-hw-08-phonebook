4c6d420d0c0bf3677bf8fcecd4604aa4528ed7de
Serhii Panasiuk <regidmail.sergej@gmail.com>
2022-05-14 00:40:47 +0300

    final-6-commit

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ root.render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <App />
         </BrowserRouter>
         {/* </PersistGate> */}
